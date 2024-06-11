17886c0081241a0b0d7d79cac53379526f96fca6
Luncert <2725115515@qq.com>
2024-06-11 20:15:56 +0800

    update sidebar

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,7 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import AppLayout from './AppLayout'
 import { Route, Router } from "@solidjs/router"
 import Home from './views/Home'
+import './App.css'
 
 function App() {
 
@@ -13,32 +10,6 @@ function App() {
       <Route path="/" />
       <Route path="/home" component={Home} />
     </Router>
-  )
-  const [count, setCount] = createSignal(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
   )
 }
 
--- example/src/AppLayout.tsx
+++ example/src/AppLayout.tsx
@@ -1,6 +1,8 @@
 import { BiSolidDashboard } from "solid-icons/bi"
 import { JSX } from "solid-js"
 import { Sidebar, SidebarItem } from "../../src/Sidebar"
+import solidLogo from './assets/solid.svg'
+import viteLogo from '/vite.svg'
 
 export default function AppLayout(props: {
   children?: JSX.Element
@@ -8,6 +10,17 @@ export default function AppLayout(props: {
   return (
     <div class="relative flex w-full h-full gap-1">
       <Sidebar class="shrink-0 p-2 gap-1 bg-zinc-900">
+        <div class="p-2">
+          <div class="flex justify-center gap-2">
+            <a href="https://vitejs.dev" target="_blank">
+              <img src={viteLogo} class="h-8 transition-all logo" alt="Vite logo" />
+            </a>
+            <a href="https://solidjs.com" target="_blank">
+              <img src={solidLogo} class="h-8 transition-all logo solid" alt="Solid logo" />
+            </a>
+          </div>
+          <h1>Vite + Solid</h1>
+        </div>
         <SidebarItem key="home" label={
           <div class="flex items-center gap-2">
             <BiSolidDashboard />
